abc1bf5692653310834ba278e9f50bcf7e8bfba9
Zheng Lu <57997950+AdamZhengLu@users.noreply.github.com>
2024-01-14 23:10:35 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -19,5 +19,6 @@ shieldio_data = {
   "label": "citations",
   "message": f"{author['citedby']}",
 }
+
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
